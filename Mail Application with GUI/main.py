8df32a2d6dc50ceab4b57eb67f779cc4708039f4
Bhavya Bhagerathi <135530387+bhavyabhagerathi@users.noreply.github.com>
2023-11-30 23:44:13 +0530

    Add files via upload

---
+++ Mail Application with GUI/main.py
@@ -0,0 +1,58 @@
+import typing
+from PyQt5.QtWidgets import *
+from PyQt5 import QtCore, uic
+from PyQt5.QtWidgets import QWidget
+
+
+import smtplib
+from email import encoders
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+
+
+class MyGUI(QMainWindow):
+    
+    def __init__(self):
+        super(MyGUI, self).__init__()
+        uic.loadUi("mailGUI.ui", self)
+        self.show()
+        
+        self.pushButton.clicked.connect(self.login)
+        self.pushButton_2.clicked.connect(self.attach_sth)
+        self.pushButton_3.clicked.connect(self.send_mail)
+        
+    
+    def login(self):
+        try:
+            self.server = smtplib.SMTP(self.lineEdit_3.text(), self.lineEdit_4.text())
+            self.server.ehlo()
+            self.server.starttls()
+            self.server.ehlo()
+            self.server.login(self.lineEdit.text(), self.lineEdit_2.text())
+            
+            self.lineEdit.setEnabled(False)
+            self.lineEdit_2.setEnabled(False)
+            self.lineEdit_3.setEnabled(False)
+            self.lineEdit_4.setEnabled(False)
+            self.pushButton.setEnabled(False)
+            
+            self.lineEdit_5.setEnabled(True)
+            self.lineEdit_6.setEnabled(True)
+            self.textEdit.setEnabled(True)
+            self.pushButton_2.setEnabled(True)
+            self.pushButton_3.setEnabled(True)
+            
+        except:
+            pass
+            
+    
+    def attach_sth(self):
+        pass
+    
+    def send_mail(self):
+        pass
+    
+app = QApplication([])
+window = MyGUI()
+app.exec_()
